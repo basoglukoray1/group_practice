424a3b9d35652d8d871bef2d053352c45e05e455
Toghrul <te7999@gmail.com>
2022-02-17 19:07:09 -0800

    Wonderful job

--- src/test/java/com/cydeo/tests/TC1.java
+++ src/test/java/com/cydeo/tests/TC1.java
@@ -3,5 +3,7 @@ package com.cydeo.tests;
 public class TC1 {
     public static void main(String[] args) {
         System.out.println("Hello dear classmates!");
+        System.out.println("Keep it up, we are doing wonderful job!");
     }
+
 }
